7098a8f3ec4f7792f47bd5f0b82082273c34e3c3
Lee <cn.pk52@gmail.com>
2017-08-27 17:11:01 +0000

    修改

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,8 +2,8 @@
     <footer class="footer">
         <small class="slogon">
             <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
-            <a href="https://yousails.com/">
-                优帆远扬 | 创造不息，交付不止
+            <a href="{{route('home')}}">
+                不要等待机会，而要创造机会。
             </a>
         </small>
         <nav>
