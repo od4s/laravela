27b869f90bb365d3939d37202b4be9730ce43265
Lee <cn.pk52@gmail.com>
2017-08-28 02:14:59 +0000

    将用户模型移动到模型文件夹

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,24 @@
 <?php
 
-namespace App;
+namespace App\Models; //修改命名空间
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable; //消息通知类功能引用
+use Illuminate\Foundation\Auth\User as Authenticatable; //授权相关功能引用
 
 class User extends Authenticatable
 {
     use Notifiable;
 
+    /*
+     * 指定和数据库交互的表名
+     */
+
+    protected $table='users';
+
     /**
      * The attributes that are mass assignable.
-     *
+     * 过滤的字段
      * @var array
      */
     protected $fillable = [
